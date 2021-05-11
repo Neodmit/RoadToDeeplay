9cc76a1851b65a8bec2162b6f5831ae7c71ba150
Neodmit <52948791+Neodmit@users.noreply.github.com>
2021-05-12 00:18:56 +0600

    Add files via upload

--- src/main/java/ExecutingСommands.java
+++ src/main/java/ExecutingСommands.java
@@ -5,22 +5,28 @@ public class ExecutingСommands {
     public static List<Integer> getResult(final String input) {
         List<Integer> result = new ArrayList<Integer>();
         int workingNumber = 0;
-        for (char symbol : input.toCharArray()) {
-            switch (symbol) {
-                case 'i':
-                    workingNumber++;
-                    break;
-                case 'd':
-                    workingNumber--;
-                    break;
-                case 's':
-                    workingNumber = (int) Math.pow(workingNumber, 2);
-                    break;
-                case 'o':
-                    result.add(workingNumber);
-                    break;
+        if (input != null) {
+            if (input.length() == 0) return null;
+            for (char symbol : input.toCharArray()) {
+                switch (symbol) {
+                    case 'i':
+                        workingNumber++;
+                        break;
+                    case 'd':
+                        workingNumber--;
+                        break;
+                    case 's':
+                        workingNumber = (int) Math.pow(workingNumber, 2);
+                        break;
+                    case 'o':
+                        result.add(workingNumber);
+                        break;
+                    default:
+                        return null;
+                }
             }
+            return result;
         }
-        return result;
+        return null;
     }
 }
--- src/test/java/TestExecutingCommands.java
+++ src/test/java/TestExecutingCommands.java
@@ -7,14 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestExecutingCommands {
 
     @Test
-    public void testDecoding1() {
-        assertEquals(Arrays.asList(8, 64), ExecutingСommands.getResult("iiisdoso"));
-    }
+    public void testDecoding1() { assertEquals(Arrays.asList(8, 64), ExecutingСommands.getResult("iiisdoso")); }
 
     @Test
-    public void testDecoding2() {
-        assertEquals(Arrays.asList(16, 14, 65536), ExecutingСommands.getResult("iissoddoiisso"));
-    }
+    public void testDecoding2() { assertEquals(Arrays.asList(16, 14, 65536), ExecutingСommands.getResult("iissoddoiisso")); }
 
     @Test
     public void testDecoding3() {
@@ -23,16 +19,38 @@ public class TestExecutingCommands {
 
     @Test
     public void testDecoding4() {
+        assertEquals(null, ExecutingСommands.getResult("DDDoSOIo"));
+    }
+
+
+    @Test
+    public void testDecoding5() {
         assertEquals(Arrays.asList(0), ExecutingСommands.getResult("o"));
     }
 
     @Test
-    public void testDecoding5() {
-        assertEquals(Arrays.asList(), ExecutingСommands.getResult(""));
+    public void testDecoding6() {
+        assertEquals(Arrays.asList(3), ExecutingСommands.getResult("iiio"));
     }
 
     @Test
-    public void testDecoding6() {
-        assertEquals(Arrays.asList(), ExecutingСommands.getResult("1"));
+    public void testDecoding7() {
+        assertEquals(null, ExecutingСommands.getResult("gdsfgdfsgf"));
     }
+
+    @Test
+    public void testDecoding8() {
+        assertEquals(null, ExecutingСommands.getResult(""));
+    }
+
+    @Test
+    public void testDecoding9() {
+        assertEquals(null, ExecutingСommands.getResult("1"));
+    }
+
+    @Test
+    public void testDecoding10() {
+        assertEquals(null, ExecutingСommands.getResult(null));
+    }
+
 }
